579b97d67affba99aa95b7740f33c1e38b0ed8b2
Yowani24 <yowani24@outlook.com>
2021-10-08 17:40:49 -0300

    new nova now

---
+++ .history/tailwind.config_20211007105707.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: 'tw-',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      textColor: ['group-hover'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
